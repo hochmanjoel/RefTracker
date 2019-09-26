4c2cb719bad08de18580bcb4cda74188a14c0204
Joel Hochman <hochmanjoel@gmail.com>
2019-09-25 20:34:39 -0500

    end of chapter 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :main_data
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'main_data#index'
 end
