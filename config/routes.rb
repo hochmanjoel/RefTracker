abcd1c1d2b47c3dd6e587639ef4ae56e425c0bef
Joel Hochman <hochmanjoel@gmail.com>
2019-10-30 22:41:17 -0500

    final edits for milestone 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, controller: 'newsletter'
-  get 'referrals/index'
+  root 'referrals#index'
   get 'referrals/details'
   get 'newsletter/sign_up/:id', to: 'newsletter#sign_up'
   post 'newsletter/sign_up/:id', to: 'newsletter#submit'
   get 'newsletter/success/:id', to: 'newsletter#success'
-  root 'referrals#index'
 end
