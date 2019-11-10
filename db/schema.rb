1f557ef497368bdd1b5cf0a4041f63636c6260ce
Ilana Seidl <iseidl@tulane.edu>
2019-11-09 19:04:56 -0600

    delete Admin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_203745) do
-
-  create_table "admins", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 2019_10_31_013322) do
 
   create_table "referrals", force: :cascade do |t|
     t.integer "referrer_id"
