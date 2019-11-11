0fbc0d2c317d260bdd944e1afe3a0ed6472f4185
Ilana Seidl <iseidl@tulane.edu>
2019-11-11 12:49:39 -0600

    create admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'admins/new'
   root 'referrals#index'
   get 'referrals/details'
   get 'newsletter/sign_up/:id', to: 'newsletter#sign_up'
   post 'newsletter/sign_up/:id', to: 'newsletter#submit'
   get 'newsletter/success/:id', to: 'newsletter#success'
+  get '/signup', to: 'admins#new'
 end
