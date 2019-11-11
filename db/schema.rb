05e7fc10be0bec4498c87ea4f0af70bc11ff2d77
Ilana Seidl <iseidl@tulane.edu>
2019-11-11 14:01:04 -0600

    edit test

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_190919) do
+ActiveRecord::Schema.define(version: 2019_11_11_194015) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
   create_table "referrals", force: :cascade do |t|
--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -14,4 +14,42 @@ class AdminTest < ActiveSupport::TestCase
     @admin.name = "     "
     assert_not @admin.valid?
   end
+
+  test "email should be present" do
+    @admin.email = "     "
+    assert_not @admin.valid?
+  end
+
+  test "name should not be too long" do
+    @admin.name = "a" * 51
+    assert_not @admin.valid?
+  end
+
+  test "email should not be too long" do
+    @admin.name = "a" * 244 + "@example.com"
+    assert_not @admin.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @admin.email = valid_address
+      assert @admin.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_admin = @admin.dup
+    duplicate_admin.email = @admin.email.upcase
+    @admin.save
+    assert_not duplicate_admin.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @admin.email = mixed_case_email
+    @admin.save
+    assert_equal mixed_case_email.downcase, @admin.reload.email
+  end
+
 end
