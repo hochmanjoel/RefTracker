a2fcbfb97af1e75772bcb2af51ad1a7cfecfbf5c
Ilana Seidl <iseidl@tulane.edu>
2019-11-14 11:02:12 -0600

    "admin controller"

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -6,4 +6,18 @@ class AdminsController < ApplicationController
   def new
     @admin = Admin.new
   end
+
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def admin_params
+      params.require(:admin).permit(:name, :email, :password,
+        :password_confirmation)
+    end
 end
