fe682df0c0ae7a9466a79bf4c3dc604ef92e0445
Ilana Seidl <iseidl@tulane.edu>
2019-12-03 14:08:24 -0600

    newsletter signup page works

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: 'referrals#index'
   get 'referrals/details'
-  get 'newsletter/sign_up', to: 'newsletter#sign_up'
-  get 'newsletter/sign_up/:id', to: 'newsletter#sign_up'
-  post 'newsletter/sign_up/:id', to: 'newsletter#submit'
-  get 'newsletter/success/:id', to: 'newsletter#success'
+  get 'newsletter/sign_up/(:id)', to: 'newsletter#sign_up'
+  post 'newsletter/sign_up/(:id)', to: 'newsletter#submit'
+  get 'newsletter/success/(:id)', to: 'newsletter#success'
 end
